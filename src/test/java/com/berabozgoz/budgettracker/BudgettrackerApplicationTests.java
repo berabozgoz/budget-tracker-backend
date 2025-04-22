dae330e61f0e8cfa0f13ff796f228285ddef687b
berabozgoz <berabozgoz@gmail.com>
2025-04-22 20:34:54 +0300

    Initial Spring Boot project setup

---
+++ src/test/java/com/berabozgoz/budgettracker/BudgettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berabozgoz.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
